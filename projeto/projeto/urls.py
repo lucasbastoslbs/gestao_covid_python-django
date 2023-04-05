ee47fe110b6d9705aadb875859363f6c6d794562
Lucas Bastos <74251978+lucasbastoslbs@users.noreply.github.com>
2023-04-05 16:01:50 -0300

    parte 2 projeto

---
+++ projeto/projeto/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('core.urls')),
+    url(r'usuario/', include('usuario.urls')),
+    url(r'unidade/', include('unidade.urls')),
+    url(r'triagem/', include('triagem.urls')),
+    url(r'medicamento/', include('medicamento.urls')),
+   
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
